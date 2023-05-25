f6fd96306ae8291cc4d3ff52f8571147f503f5bb
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-24 23:20:48 -0400

    Tour and Loading state setup
    - set up loading and tours state with tours passing an empty object

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import Loading from './Loading'
 import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://course-api.com/react-tours-project' //personal API setup
+
 function App() {
+  const [loading, setLoading] = useState(true); //This is so we'll see the loading component
+  const [tours, setTours] = useState([]);
+  if (loading){
+    return <main>
+       <Loading /> {/* becasue loading is set to true it is displayed */}
+    </main>
+  }
   return <h2>Tours Project Setup</h2>
 }
 
