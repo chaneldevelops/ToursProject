7df23d9b6572e11a5f9d683c7f03b82136b9c35b
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-25 00:08:29 -0400

    Can see name and price
    - displayed tour info, name and price in the div

--- src/Tour.js
+++ src/Tour.js
@@ -4,7 +4,10 @@ const Tour = ({id,image, info, price,name}) => {
   return <article className='single-tour'>
     <img src={image} alt={name} />
     <footer>
-      <div className='tour-info'></div>
+      <div className='tour-info'>
+        <h4>{name}</h4>
+        <h4 className='tour-price'>${price}</h4>
+      </div>
     </footer>
   </article>;
 };
