fc04e73eabab4e0d20f4a9c658abcf9e11432319
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-25 01:08:39 -0400

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
+import Navbar from './Navbar'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project' //personal API setup
