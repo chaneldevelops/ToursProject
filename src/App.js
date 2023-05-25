fd323538b80bb821a544ffa8c8a8c211115df3be
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-24 23:35:01 -0400

    Update App.js
    - added an if statement with the loading component
    - changed tours project to display the tours component
    -  created an async with fetch

--- src/App.js
+++ src/App.js
@@ -8,12 +8,25 @@ const url = 'https://course-api.com/react-tours-project' //personal API setup
 function App() {
   const [loading, setLoading] = useState(true); //This is so we'll see the loading component
   const [tours, setTours] = useState([]);
-  if (loading){
-    return <main>
-       <Loading /> {/* becasue loading is set to true it is displayed */}
-    </main>
+
+
+  const fetchTours = async () => {
+    setLoading(true) //Can be an extra precaution for loading to show when fetch the data
+    const response = await fetch(url);
   }
-  return <h2>Tours Project Setup</h2>
+
+  if (loading){
+    return (
+      <main>
+        <Loading /> {/* becasue loading is set to true it is displayed */}
+      </main>
+    );
+  }
+  return (
+    <main>
+       <Tours /> {/*Need a return if we're not loading */}
+    </main>
+  )
 }
 
 export default App
